ebc9976684a6100f45b6136d1a32124259bcb1f9
woods407 <w_dorcin@yahoo.comm>
2021-02-07 20:07:44 -0500

    new push

---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes=[
+    "If you want something you have never had, you must be willing to do something you have never done.",
+    
+    "Be the change you wish to see in the world." 
+] 
+let index= Math.floor(Math.random() * 2)
+console.log(quotes[index])
